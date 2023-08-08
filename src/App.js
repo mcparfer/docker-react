ad9dca0a4f1aa671ab4a1a1a8fc55efd01d09fb8
mcparfer <mcparfer@gmail.com>
2023-08-08 20:29:11 +0200

    changed app text x3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          socorro :)
+          Amo a Edgeworth.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Ya no quiero aprender react.
+          Learn React
         </a>
       </header>
     </div>
